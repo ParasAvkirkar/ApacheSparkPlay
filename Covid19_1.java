d2d87e8a4c3affebd46e6345d8d6e498c029417b
ParasAvkirkar <parasavkirkar96@gmail.com>
2020-04-23 15:07:40 -0700

    implemented task-1 to estimate countries with their coronoa case stats

--- Covid19_1.java
+++ Covid19_1.java
@@ -1,4 +1,11 @@
+
+
+import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import org.apache.commons.lang.WordUtils;
@@ -10,56 +17,147 @@ import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 
 public class Covid19_1 {
+	//TODO: Remove unnecessary methods
 	
 	// 4 types declared: Type of input key, type of input value, type of output key, type of output value
-	public static class MyMapper extends Mapper<Object, Text, Text, LongWritable> {
-		private final static LongWritable one = new LongWritable(1);
-		private Text word = new Text();
+	public static class CaseMapper extends Mapper<Object, Text, Text, CountryStatWritable> {
+
+		public static boolean isWorld;
+		
+		private Date parseDate(String date) {
+			Date result = null;
+			
+			DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		
+			try {
+				result = format.parse(date);
+			} catch (ParseException e) {
+				// no handles
+			}
+			
+			return result;
+		}
 		
 		// The 4 types declared here should match the types that was declared on the top
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			StringTokenizer tok = new StringTokenizer(value.toString(), ";\"\'. \t‘“"); 
-			while (tok.hasMoreTokens()) {
-				word.set(tok.nextToken());
-				context.write(word, one);
+			StringTokenizer columnTokens = new StringTokenizer(value.toString(), ",");
+			String columns[] = new String[4];
+			int i = 0;
+			while (i < 4 && columnTokens.hasMoreTokens()) {
+				columns[i] = columnTokens.nextToken();
+				++i;
+			}
+		
+			// processing only when non header rows encounterd
+			if (!"date".equals(columns[0])) {
+				if (!isWorld && "world".equals(columns[1].toLowerCase())) {
+					return;
+				}
+				
+				Date date = parseDate(columns[0]);
+				Date oldDateFilter = parseDate("2019-12-31");
+				// considering dates only after Dec, 12, 2019
+				if (date != null && oldDateFilter != null && date.after(oldDateFilter)) { 
+					// created a Writable class to hold the country-name and case-counts
+					CountryStatWritable stat = new CountryStatWritable(columns[1], Long.parseLong(columns[2]));
+					context.write(new Text(columns[1]), stat);
+				}
 			}
 		}
 		
 	}
 	
 	
-
 	// 4 types declared: Type of input key, type of input value, type of output key, type of output value
 	// The input types of reduce should match the output type of map
-	public static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+	public static class CountReducer extends Reducer<Text, CountryStatWritable, Text, LongWritable> {
 		private LongWritable total = new LongWritable();
 		
 		// Notice the that 2nd argument: type of the input value is an Iterable collection of objects 
 		//  with the same type declared above/as the type of output value from map
-		public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
-			long sum = 0;
-			for (LongWritable tmp: values) {
-				sum += tmp.get();
+		public void reduce(Text key, Iterable<CountryStatWritable> values, Context context) throws IOException, InterruptedException {
+			long totalCases = 0;
+			for (CountryStatWritable stat: values) {
+				totalCases += stat.getCaseCount().get();
 			}
-			total.set(sum);
-			// This write to the final output
+			
+			total.set(totalCases);
+		
 			context.write(key, total);
 		}
 	}
 	
 	
 	public static void main(String[] args)  throws Exception {
+		boolean isWorld = Boolean.parseBoolean(args[1]);
+		CaseMapper.isWorld = isWorld;
+
 		Configuration conf = new Configuration();
-		Job myjob = Job.getInstance(conf, "my word count test");
-		myjob.setJarByClass(MyWordCount.class);
-		myjob.setMapperClass(MyMapper.class);
-		myjob.setReducerClass(MyReducer.class);
-		myjob.setOutputKeyClass(Text.class);
-		myjob.setOutputValueClass(LongWritable.class);
-		// Uncomment to set the number of reduce tasks
-		// myjob.setNumReduceTasks(2);
-		FileInputFormat.addInputPath(myjob, new Path(args[0]));
-		FileOutputFormat.setOutputPath(myjob,  new Path(args[1]));
-		System.exit(myjob.waitForCompletion(true) ? 0 : 1);
+		Job jobInstance = Job.getInstance(conf, "Total count of reported cases");
+		
+		jobInstance.setJarByClass(Covid19_1.class);
+		jobInstance.setMapperClass(CaseMapper.class);
+		jobInstance.setReducerClass(CountReducer.class);
+
+		jobInstance.setMapOutputKeyClass(Text.class);
+		jobInstance.setMapOutputValueClass(CountryStatWritable.class);
+
+		jobInstance.setOutputKeyClass(Text.class);
+		jobInstance.setOutputValueClass(LongWritable.class);
+		
+		FileInputFormat.addInputPath(jobInstance, new Path(args[0]));
+		FileOutputFormat.setOutputPath(jobInstance,  new Path(args[2]));
+		System.exit(jobInstance.waitForCompletion(true) ? 0 : 1);
+	}
+}
+
+class CountryStatWritable implements Writable {
+	
+	private Text countryName;
+	private LongWritable caseCount;
+	
+	public CountryStatWritable(String countryName, long caseCount) {
+		this.countryName = new Text(countryName);
+		this.caseCount = new LongWritable(caseCount);
+	}
+
+	public CountryStatWritable() {
+		this.countryName = new Text();
+		this.caseCount = new LongWritable();
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		// TODO Auto-generated method stub
+		countryName.write(out);
+		caseCount.write(out);
+	}
+	
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		// TODO Auto-generated method stub
+		countryName.readFields(in);
+		caseCount.readFields(in);
+	}
+
+	public Text getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(Text countryName) {
+		this.countryName = countryName;
+	}
+
+	public LongWritable getCaseCount() {
+		return caseCount;
+	}
+
+	public void setCaseCount(LongWritable caseCount) {
+		this.caseCount = caseCount;
+	}
+
+	@Override
+	public String toString() {
+		return "CountryStat [countryName=" + countryName + ", caseCount=" + caseCount + "]";
 	}
 }
